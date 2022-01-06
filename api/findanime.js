35ed0d17ab524c4cb889eb0b33277030202e24bb
madtisa <97251780+madtisa@users.noreply.github.com>
2022-01-07 01:44:04 +0300

    Add initial findanime api

---
+++ api/findanime.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name     FindAnime API
+// @version  1.0.0
+// @match https://readmanga.io/*
+// @match https://findanime.net/*
+// @grant    none
+// ==/UserScript==
+
+function parseCurrentArticle() {
+  const namesWrapper = document.querySelector('.names');
+
+  if (!namesWrapper) return null;
+
+  const namesList = Array.from(namesWrapper.children)
+    .map((name) => ({ name: name.innerText, tag: name.classList }));
+  const articleUrl = new URL(document.querySelector('meta[itemprop=url]').content);
+  const articleId = articleUrl.pathname.split('/').reverse()[0];
+
+  return {
+    id: articleId,
+    url: articleUrl,
+    names: namesList,
+  }
+}
+
